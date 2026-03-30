71faef01ac948c4c02ea253b18961195843da412
Mihai Olaru <ecstrim@users.noreply.github.com>
2026-03-30 11:13:45 +0200

    event-hub v2.0.0: PE naming override with pep- default (CAF)

--- modules/event-hub/main.tf
+++ modules/event-hub/main.tf
@@ -76,13 +76,14 @@ resource "azurerm_eventhub_namespace_authorization_rule" "this" {
 resource "azurerm_private_endpoint" "this" {
   count = var.enable_private_endpoint ? 1 : 0
 
-  name                = "pe-${var.name}"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  subnet_id           = var.subnet_id
+  name                          = coalesce(var.private_endpoint_name, "pep-${var.name}")
+  location                      = var.location
+  resource_group_name           = var.resource_group_name
+  subnet_id                     = var.subnet_id
+  custom_network_interface_name = coalesce(var.private_endpoint_nic_name, "pep-${var.name}-nic")
 
   private_service_connection {
-    name                           = "psc-${var.name}"
+    name                           = coalesce(var.private_service_connection_name, "psc-${var.name}")
     private_connection_resource_id = azurerm_eventhub_namespace.this.id
     subresource_names              = ["namespace"]
     is_manual_connection           = false
